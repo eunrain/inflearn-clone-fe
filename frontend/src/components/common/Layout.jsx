ae4d9d2c1a8d8e910784ad42b8b050fd7e605532
goeunrain <boniteterain@gmail.com>
2022-10-28 21:16:17 +0900

    header 완성

--- frontend/src/components/common/Layout.jsx
+++ frontend/src/components/common/Layout.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import Header from "./Header";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <StLayout>
+      <Header />
+      {children}
+    </StLayout>
+  );
 };
 
 export default Layout;
+
+const StLayout = styled.div`
+  padding-top: 20px;
+  width: 100%;
+  height: 100%;
+  border-bottom: 1px solid #eeeeef;
+  box-shadow: 0px 13px 12px -15px #eeeeef;
+`;
