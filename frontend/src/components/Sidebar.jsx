1a9cf54d3678d7505a4d53ec2b75059cb367d4d0
goeunrain <boniteterain@gmail.com>
2022-10-29 16:41:27 +0900

    sidebar 진행중

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  const StLinkTitle = styled(Link)`
+    display: block;
+    text-align: center;
+    padding: 29px 0;
+    border-bottom: 1px solid #e4e4e4;
+    font-size: 20px;
+  `;
+  const StLink = styled(Link)`
+    display: block;
+    text-align: center;
+    padding: 24px 0;
+    font-size: 20px;
+    border-bottom: 1px solid #e4e4e4;
+  `;
+
+  const StLinkEnd = styled(Link)`
+    display: block;
+    text-align: center;
+    padding: 25px 0;
+    font-size: 20px;
+  `;
+
+  return (
+    <StContainer>
+      <StWrap>
+        <StLinkTitle to="/signup">전체 강의</StLinkTitle>
+        <StLink to="/login">Full Stack</StLink>
+        <StLink>FrontEnd </StLink>
+        <StLinkEnd>BackEnd</StLinkEnd>
+      </StWrap>
+    </StContainer>
+  );
+};
+
+export default Sidebar;
+
+const StContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: start;
+  margin: 50px 0 0 60px;
+`;
+
+const StWrap = styled.div`
+  width: 240px;
+  height: 300px;
+  border: 1px solid #e4e4e4;
+  background-color: #fafafa;
+`;
