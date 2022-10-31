71f76e85e5ed3a31a451cd863fa0c3ee5f5d6c8c
goeunrain <boniteterain@gmail.com>
2022-10-31 20:25:59 +0900

    호버어렵다

--- frontend/src/components/PostCard1.jsx
+++ frontend/src/components/PostCard1.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import styled from "styled-components";
+import {
+  BsSuitHeartFill,
+  BsSuitHeart,
+  BsCartPlus,
+  BsCartXFill,
+} from "react-icons/bs";
 
-const PostCard1 = () => {
+const PostCard1 = ({ post }) => {
   return (
     <Container>
-      <Img></Img>
-      <Title>postcard1</Title>
-      <Name></Name>
-      <Price></Price>
+      <StPostCard1>
+        <Img src={post.thumbnail}></Img>
+        <Title>{post.title}</Title>
+        <Name>{post.tutor}</Name>
+        <Price>{post.price}</Price>
+      </StPostCard1>
+      <StPostCard2>
+        <HoverTitle>postcard2</HoverTitle>
+        <HoverDescrip></HoverDescrip>
+        <HoverTag></HoverTag>
+        <BsSuitHeart size="28" />
+        <BsSuitHeartFill size="28" />
+        <BsCartPlus size="30" />
+        <BsCartXFill size="30" />
+      </StPostCard2>
     </Container>
   );
 };
@@ -42,4 +59,45 @@ const Price = styled.div`
   width: 290px;
   height: 55px;
   border: 1px solid orange;
+`;
+
+// PostCard After
+
+const HoverTitle = styled.div`
+  width: 290px;
+  height: 80px;
+  border: 1px solid blue;
+`;
+
+const HoverDescrip = styled.div`
+  width: 290px;
+  height: 140px;
+  border: 1px solid green;
+`;
+
+const HoverTag = styled.div`
+  width: 290px;
+  height: 55px;
+  border: 1px solid orange;
+`;
+
+const HoverIcons = styled.div`
+  display: flex;
+`;
+
+const StPostCard2 = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.6);
+  opacity: 0;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const StPostCard1 = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  position: absolute;
 `;
